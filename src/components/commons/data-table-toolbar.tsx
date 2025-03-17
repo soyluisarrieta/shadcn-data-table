18895da4d8b8f802d3f0d37bea61abeb0511d7f2
soyluisarrieta <luisarrieta796@gmail.com>
2025-03-17 15:51:06 -0500

    feat: add email input search from Shadcn docs

---
+++ src/components/commons/data-table-toolbar.tsx
@@ -0,0 +1,21 @@
+import { Input } from '@/components/ui/input'
+import { Table } from '@tanstack/react-table'
+
+interface DataTableToolBarProps <TData> {
+  table: Table<TData>
+}
+
+export default function DataTableToolbar<TData> ({ table }: DataTableToolBarProps<TData>) {
+  return (
+    <div className="flex items-center py-4">
+      <Input
+        placeholder="Filter emails..."
+        value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
+        onChange={(event) =>
+          table.getColumn('email')?.setFilterValue(event.target.value)
+        }
+        className="max-w-sm"
+      />
+    </div>
+  )
+}
--- src/components/commons/data-table.tsx
+++ src/components/commons/data-table.tsx
@@ -1,7 +1,9 @@
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   SortingState,
@@ -18,6 +20,7 @@ import {
 } from '@/components/ui/table'
 import DataTableFooter from '@/components/commons/data-table-footer'
 import { useState } from 'react'
+import DataTableToolbar from '@/components/commons/data-table-toolbar'
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -29,66 +32,73 @@ export function DataTable<TData, TValue> ({
   data
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
 
   const table = useReactTable({
     data,
     columns,
     onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     state: {
-      sorting
+      sorting,
+      columnFilters
     }
   })
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
+    <>
+      <DataTableToolbar table={table} />
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
 
-                console.log(header.getSize())
-                return (
-                  <TableHead key={header.id} style={{ width: header.getSize(), maxWidth: header.getSize() }}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id} style={{ width: cell.column.getSize(), maxWidth: cell.column.getSize() }}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+                  console.log(header.getSize())
+                  return (
+                    <TableHead key={header.id} style={{ width: header.getSize(), maxWidth: header.getSize() }}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  )
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <DataTableFooter table={table} />
-    </div>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} style={{ width: cell.column.getSize(), maxWidth: cell.column.getSize() }}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <DataTableFooter table={table} />
+      </div>
+    </>
   )
 }
