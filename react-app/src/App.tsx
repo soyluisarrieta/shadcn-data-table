c468aa5367b57d4c213504e2d1df6273bd8c598f
soyluisarrieta <luisarrieta796@gmail.com>
2025-04-09 13:11:43 -0500

    feat: add work in progress banner with external link

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,23 @@
 import DemoPayments from '@/app/payment/payments-demo'
 import { ToggleTheme } from '@/components/commons/toggle-theme'
+import { ExternalLinkIcon } from 'lucide-react'
 
 export default function App () {
   return (
     <main>
-      <section className="container-wrapper py-20">
+      <section className="container-wrapper py-20 flex flex-col items-center">
+        <div className="relative">
+          <a target="__blank" className="group relative mb-8 inline-block cursor-pointer rounded-full bg-border p-px text-xs leading-6 font-semibold text-foreground shadow-sm dark:shadow-none shadow-amber-900/20" href="https://github.com/soyluisarrieta/shadcn-data-table">
+            <span className="absolute inset-0 overflow-hidden rounded-full">
+              <span className="absolute inset-0 rounded-full bg-gradient-to-r from-amber-400/0 via-amber-400/90 to-amber-400/0 opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+            </span>
+            <div className="relative z-10 flex items-center space-x-2 rounded-full bg-background px-4 py-0.5 ring-1 ring-white/10">
+              <span>🚧 Work in Process</span>
+              <ExternalLinkIcon size={12} />
+            </div>
+            <span className="absolute -bottom-0 left-1/2 -translate-x-1/2 h-px w-1/2 bg-gradient-to-r from-amber-400/0 via-amber-400/90 to-amber-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+          </a>
+        </div>
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-700 dark:from-neutral-50 dark:to-neutral-400 bg-opacity-50">
           Shadcn Data Table
         </h1>
