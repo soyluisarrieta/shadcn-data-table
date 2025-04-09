214cc4e5bb29e9674c1e2e3454dccb70a67c3e52
soyluisarrieta <luisarrieta796@gmail.com>
2025-04-08 20:01:03 -0500

    feat(meta-tags): add meta tags constants and update layout

---
+++ www/src/constants/meta-tags.ts
@@ -0,0 +1,15 @@
+export const META_TAGS =
+{
+  TITLE: 'Shadcn Data Table | @soyluisarrieta',
+  DESCRIPTION: {
+    LG: 'A powerful and versatile data table component featuring advanced filtering capabilities, straightforward implementation process, extensive customization options, and a fully responsive design built on top of the robust Shadcn component library.',
+    MD: 'A versatile data table component with advanced filtering, easy implementation, and responsive design built on Shadcn components.',
+  },
+  DOMAIN: 'datatable-soyluisarrieta.vercel.app',
+  URL: 'https://datatable-soyluisarrieta.vercel.app/',
+  IMAGE: 'https://datatable-soyluisarrieta.vercel.app/og.png',
+  GITHUB: {
+    URL: 'https://github.com/soyluisarrieta/shadcn-data-table',
+    USERNAME: '@soyluisarrieta',
+  }
+}
