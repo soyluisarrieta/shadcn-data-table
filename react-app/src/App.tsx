90f67e1ce166382f103f9acfbe7e72507822c5b7
soyluisarrieta <luisarrieta796@gmail.com>
2025-04-25 13:35:36 -0500

    style: adjust text size and padding for better responsiveness

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -18,7 +18,7 @@ export default function App () {
             <span className="absolute -bottom-0 left-1/2 -translate-x-1/2 h-px w-1/2 bg-gradient-to-r from-amber-400/0 via-amber-400/90 to-amber-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
           </a>
         </div>
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-700 dark:from-neutral-50 dark:to-neutral-400 bg-opacity-50">
+        <h1 className="text-5xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-700 dark:from-neutral-50 dark:to-neutral-400 bg-opacity-50">
           Shadcn Data Table
         </h1>
         <p className="mt-4 mb-10 font-normal text-base text-muted-foreground max-w-lg text-center mx-auto">
@@ -29,8 +29,10 @@ export default function App () {
         <div className="border-y border-dashed">
           <div className='container-wrapper p-6 text-center flex justify-between'>
             <a className='flex items-center gap-2 w-fit text-muted-foreground font-light text-sm tracking-wide' href="https://github.com/soyluisarrieta/shadcn-data-table" target='_blank'>
-              @soyluisarrieta/shadcn-data-table
-              <div className='w-px h-6 mx-2 border-r border-muted' />
+              <span className='hidden sm:flex gap-2'>
+                @soyluisarrieta/shadcn-data-table
+                <div className='w-px h-6 mx-2 border-r border-muted' />
+              </span>
               <img
                 className='h-5 object-contain mt-0.5'
                 src='https://img.shields.io/github/stars/soyluisarrieta/shadcn-datatable.svg?style=social&label=Stars&maxAge=2592000'
@@ -42,7 +44,7 @@ export default function App () {
           </div>
         </div>
 
-        <div className="container-wrapper p-6">
+        <div className="container-wrapper p-3 md:p-6">
           <div className='bg-background p-4 md:p-10 rounded-lg border'>
             <DemoPayments />
           </div>
